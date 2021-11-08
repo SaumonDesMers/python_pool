8f9e0bdb64ee7628052d9558c1ecd362e5223a69
sgaubert <sim.gaubert.sg@gmail.com>
2021-11-08 14:26:07 +0100

    pool python init

---
+++ module_00/ex01/exec.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import sys
+
+arg = sys.argv[1:]
+arg = [arg[i][::-1].swapcase() for i in range(len(arg))]
+arg.reverse()
+print(*arg, sep=' ')
---
+++ module_00/ex02/whois.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def main():
+    if len(sys.argv) == 1:
+        return
+    arg = sys.argv[1]
+    if arg.isdigit() is False or len(sys.argv) > 2:
+        print("ERROR")
+        return
+    if int(arg) == 0:
+        print("I'm Zero")
+    elif int(arg) % 2 == 0:
+        print("I'm Even")
+    else:
+        print("I'm Odd")
+
+
+main()
